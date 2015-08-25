6caa35f0d1964f3092020850b180420b686cf42b
3mjn3m <3mjn3m@gmail.com>
2015-08-25 15:59:16 +0700

    3mjn3m

---
+++ index.php
@@ -0,0 +1,71 @@
+<?php require_once('Connections/conn.php'); ?>
+<?php
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+
+mysql_select_db($database_conn, $conn);
+$query_Recordset1 = "SELECT * FROM phong";
+$Recordset1 = mysql_query($query_Recordset1, $conn) or die(mysql_error());
+$row_Recordset1 = mysql_fetch_assoc($Recordset1);
+$totalRows_Recordset1 = mysql_num_rows($Recordset1);
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<table border="1">
+  <tr>
+    <td width="146">username</td>
+    <td width="148">password</td>
+    <td width="120">email</td>
+    <td width="126">phone</td>
+  </tr>
+  <?php do { ?>
+    <tr>
+      <td><?php echo $row_Recordset1['username']; ?></td>
+      <td><?php echo $row_Recordset1['password']; ?></td>
+      <td><?php echo $row_Recordset1['email']; ?></td>
+      <td><?php echo $row_Recordset1['phone']; ?></td>
+    <td width="157"><a href="insert.php">Insert</a> 
+    <a href="update.php?phongdv=<?php echo $row_Recordset1['username']; ?>">Update</a> 
+    <a href="delete.php?phongdv=<?php echo $row_Recordset1['username']; ?>">Delete</a></td>
+    </tr>
+    <?php } while ($row_Recordset1 = mysql_fetch_assoc($Recordset1)); ?>
+</table>
+</body>
+</html>
+<?php
+mysql_free_result($Recordset1);
+?>
